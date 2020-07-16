6c95207097e155d571b08dcb5983cc982d97f900
Jaime Niswonger <jniswonger@keyholesoftware.com>
2020-07-15 23:26:06 -0500

    added popular and tinkered with style

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 
 import './App.scss';
 import { NowPlaying } from './NowPlaying';
+import { Popular } from './Popular';
 
 const App = () => {
 
   return (
     <div className="app">
       <NowPlaying />
+      <Popular />
     </div>
   );
 }
--- app/src/Popular.tsx
+++ app/src/Popular.tsx
@@ -3,14 +3,13 @@ import React from 'react';
 import Slider from './components/NetflixSlider/Slider';
 import Item from './components/NetflixSlider/Item';
 
-import movies from './mock/now_playing.json';
-import './App.scss';
+import movies from './mock/popular.json';
 
-export const NowPlaying = () => {
+export const Popular = () => {
 
     return (
         <div>
-            <h2 className="heading__slider">Now Playing</h2>
+            <h2 className="heading__slider">Popular</h2>
             <Slider>
                 {movies.map(movie => (
                     <Item movie={movie} key={movie.id}></Item>
