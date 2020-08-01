c28b85e648d9cb7e8ebd47fd166e47a1fa86cb2b
Mat Warger <mwarger@gmail.com>
2020-08-01 10:04:55 -0500

    updated styles

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -3,32 +3,69 @@ import React from 'react';
 import { MuiThemeProvider } from '@material-ui/core/styles';
 import { theme } from 'theme/Theme';
 
-import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from 'react-router-dom';
 
 import { MenuAppBar } from 'components/MenuAppBar';
 import { NowPlaying } from 'components/NowPlaying';
 import { Popular } from 'components/Popular';
 
-import { ApolloProvider } from "./context/ApolloProvider";
+import { ApolloProvider } from './context/ApolloProvider';
 
 import 'App.scss';
+import { Box, Link } from '@material-ui/core';
 
 export const App = () => {
-
   return (
     <div className="app">
       <ApolloProvider>
         <MuiThemeProvider theme={theme}>
           <Router>
-
-            <MenuAppBar />
-
-            <Switch>
-              <Route exact={true} path="/nowPlaying" component={NowPlaying} />
-              <Route exaxt={true} path="/popular" component={Popular} />
-              <Redirect from="/" exact to="/nowPlaying" />
-            </Switch>
-
+            <Box
+              style={{
+                minHeight: '100%',
+                display: 'grid',
+                gridTemplateRows: 'auto 1fr auto',
+                gridTemplateColumns: '100%',
+              }}
+            >
+              <MenuAppBar />
+              <Box>
+                <Switch>
+                  <Route
+                    exact={true}
+                    path="/nowPlaying"
+                    component={NowPlaying}
+                  />
+                  <Route exaxt={true} path="/popular" component={Popular} />
+                  <Redirect from="/" exact to="/nowPlaying" />
+                </Switch>
+              </Box>
+              <Box p={2}>
+                <Box color="#222">
+                  Icons made by{' '}
+                  <Link
+                    href="https://www.flaticon.com/authors/freepik"
+                    title="Freepik"
+                    color="inherit"
+                  >
+                    Freepik
+                  </Link>{' '}
+                  from{' '}
+                  <Link
+                    color="inherit"
+                    href="https://www.flaticon.com/"
+                    title="Flaticon"
+                  >
+                    www.flaticon.com
+                  </Link>
+                </Box>
+              </Box>
+            </Box>
           </Router>
         </MuiThemeProvider>
       </ApolloProvider>
